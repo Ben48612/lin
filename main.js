93b24da4a709710bce337e79d98c67273dbffa37
Ben48612 <ramanazb@gmail.com>
2024-07-07 15:44:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+et currentSlideID = 0;
+sliderElement = document.getElementById('slider');
+listElement = document.getElementsByTagName("li")
+totalSliders = sliderElement.childElementCount;
+console.log(totalSliders)
+function next() {
+    if (totalSliders.length < currentSlideID) {
+        currentSlideID++;
+        showSlide()
+    }
+    console.log("next is pressed");
+
+}
+function prev() {
+    if (currentSlideID.length > 1) {
+        currentSlideID--;
+        showSlide()
+    }
+    console.log("prev is pressed");
+
+}
+function showSlide() {
+    slides = sliderElement.listElement
+    for (let index = 0; index < totalSliders; index++) {
+        const element = slides[index];
+        if (currentSlideID === index + 1) {
+            element.classList.remove('hidden')
+        } else {
+            element.classList.add('hidden')
+        }
+    }
+}
+
---
+++ script.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        document.getElementById('form-response').textContent = 'Your message has been sent!';
+    });
+});
